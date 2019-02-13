a7cf74e0f185e7d7c1c2997864ba94c17aac63bf
Luciano Júnior <luciannojunior@gmail.com>
2019-02-13 21:59:11 -0200

    Setting up the backbone of the app

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject achord "0.1.0-SNAPSHOT"
+  :description "Get data from songs like chords and lyrics and also search all kinds of songs by name or artist."
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :main ^:skip-aot achord.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
